60473304c8f60f89e5b188e4067742d4e2b43759
Mxintro <986695652@qq.com>
2021-02-27 01:16:21 +0800

    func: debounce

---
+++ debounce.js
@@ -0,0 +1,18 @@
+function debounce(fn, delay){
+  let timer = null
+  return function(){
+    const that = this
+    if(timer){
+      global.clearTimeout(timer)
+    }
+    timer = setTimeout(_ => {
+      fn.apply(that, arguments)
+      timer = null
+    }, delay) 
+  }
+}
+
+const debounced = debounce(()=>console.log('hello'),1000)
+debounced()
+debounced()
+setTimeout(_ => debounced(), 1100)
